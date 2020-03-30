1a1c4a8f13bbf5e5af7611150897c21ba49d2315
Esteban Boucher <es.boucher@protomail.com>
2020-03-30 12:04:16 +0200

    Added Pokemon class with name only

---
+++ src/pokemon.ts
@@ -0,0 +1,12 @@
+
+class Pokemon {
+    name: String
+  
+    constructor(name: String) {
+      this.name = name
+    }
+  }
+  
+  let pokemon = new Pokemon("Charizard")
+  
+  console.log(pokemon)
